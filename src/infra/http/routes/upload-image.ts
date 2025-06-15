df8e1ac1969ad0cba8d8ef4dab9b2bdfc6b7ebd2
FernandoBatoni <nandobatoni22@gmail.com>
2025-06-15 12:56:40 -0300

    feat: created tests and route to get all uploads

--- src/infra/http/routes/upload-image.ts
+++ src/infra/http/routes/upload-image.ts
@@ -10,6 +10,7 @@ export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
       schema: {
         summary: 'Upload an image',
         consumes: ['multipart/form-data'],
+        tags: ['uploads'],
         response: {
           201: z.null().describe('Image uploaded'),
           400: z.object({ message: z.string() }),
@@ -26,10 +27,14 @@ export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
       }
 
       const result = await uploadImage({
-        fileName: uploadedFile?.filename,
+        fileName: uploadedFile.filename,
         contentType: uploadedFile.mimetype,
         contentStream: uploadedFile.file,
       })
+
+      if (uploadedFile.file.truncated) {
+        return reply.status(400).send({ message: 'File size limit reached.' })
+      }
 
       if (isRight(result)) {
         console.log(unwrapEither(result))
